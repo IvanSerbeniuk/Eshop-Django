b69cea2f32ebb0de9c715a0c28c9879efed33768
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-21 17:36:03 +0200

    Postgres DB and static files

--- eshop/settings.py
+++ eshop/settings.py
@@ -90,12 +90,31 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Djangoproject',
+        'USER': 'DjangoDb',
+        'PASSWORD': config('DB_Password'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+
+
+
+
 
 
 # Password validation
