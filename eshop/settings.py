c663d97ccacf8baeeb5d6e5de4b2fd911991a9b7
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-21 17:56:40 +0200

    static

--- eshop/settings.py
+++ eshop/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from decouple import config
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,6 +152,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = 'static/'
 
