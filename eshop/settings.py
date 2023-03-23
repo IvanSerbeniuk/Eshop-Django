8a9ea18588d051f7f4b7d088827cf91c04a73a46
IvanSerbeniuk <serbeniuk.ivan@gmail.com>
2023-03-23 17:26:12 +0200

    static files

--- eshop/settings.py
+++ eshop/settings.py
@@ -153,7 +153,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+#Stackoverflow
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
 
